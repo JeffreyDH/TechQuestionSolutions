c8d635ea753ca5a4e34dbe30158dc2c049b49e0a
Jeffrey Hernandez <jeffredh@usc.edu>
2018-12-14 20:24:47 -0800

    Created a folder for all geekforgeeks solutions. Primarily will use python.

---
+++ geekforgeeks/easearray.py
@@ -0,0 +1,32 @@
+def solver():
+    array_size = int(input())
+    array = [int(x) for x in input().split()]
+    
+    ease_array(array_size, array)
+    
+    
+def ease_array(size, array):
+    num_zeroes = 0
+    for i in range(size):
+        if i != (size - 1):
+            if(array[i] == array[i+1]) and (array[i] != 0):
+                array[i] *= 2
+                array[i+1] = 0
+                
+        if array[i] != 0:
+            print(array[i], end=' ', flush=True)
+
+        else:
+            num_zeroes += 1
+        
+    for i in range(num_zeroes):
+        print(0, end=' ', flush=True)
+        
+
+def main():
+    tests = int(input())
+    for i in range(tests):
+        solver()
+        # for the new line.
+        print()
+main()
